68409429c8fa3b52b0be99d8622dfdc49245fa7b
iremozkal <i.iremozkal@gmail.com>
2021-03-26 21:07:49 +0300

    payto ve broadcast kullanıldı.

--- index.js
+++ index.js
@@ -29,34 +29,34 @@ const getInfo = new Promise((resolve, reject) => {
 // cüzdan yoksa oluşturmaktır.
 
 const createWallet = () => new Promise((resolve, reject) => {
-  exec(`electrum --testnet create`, (err, data)=> {
+  exec(`electrum --testnet create`, (err, data) => {
     if (err) {
       reject(err);
-      }
-    
-      resolve(data);
+    }
+
+    resolve(data);
   });
 });
 
 // tanımlanabilir bir cüzdan var mı?
 const loadWallet = () => new Promise((resolve, reject) => {
-  exec(`electrum --testnet load_wallet`, (err, data)=> {
+  exec(`electrum --testnet load_wallet`, (err, data) => {
     if (err) {
       reject('load error');
-      }
-    
-      resolve(data);
+    }
+
+    resolve(data);
   });
 });
 
 // cüzdan varsa adres oluştur.
 const createNewAddress = () => new Promise((resolve, reject) => {
-  exec(`electrum --testnet createnewaddress`, (err, data)=> {
+  exec(`electrum --testnet createnewaddress`, (err, data) => {
     if (err) {
       reject('new adddress error');
-      }
-    
-      resolve(data);
+    }
+
+    resolve(data);
   });
 });
 
@@ -67,22 +67,28 @@ const createNewAddress = () => new Promise((resolve, reject) => {
 //Get balance
 
 const getBalance = () => new Promise((resolve, reject) => {
-  exec(`electrum --testnet getbalance`, (err, data)=> {
+  exec(`electrum --testnet getbalance`, (err, data) => {
     if (err) {
       reject(err);
-      }
-    
-      resolve(data);
+    }
+
+    resolve(data);
   });
 });
 
-
-
-/*
 // Create new wallet address
-
 const payto = (address, amount) => new Promise((resolve, reject) => {
-  exec(`electrum --testnet payto ${address} ${amount}`, (err, data)=> {
+  exec(`electrum --testnet payto ${address} ${amount}`, (err, data) => {
+    if (err) {
+      reject(err);
+    }
+
+    resolve(data);
+  });
+});
+
+const broadcast = (bytecode) => new Promise((resolve, reject) => {
+  exec(`electrum --testnet broadcast ${bytecode}`, (err, data)=> {
     if (err) {
       reject(err);
       }
@@ -90,34 +96,11 @@ const payto = (address, amount) => new Promise((resolve, reject) => {
       resolve(data);
   });
 });
-
-payto(dsadsa,0.01).then(data => {
-  console.log(data);
-}).catch(err => console.error(err));
-
-const broadcast = (transactionId) => new Promise((resolve, reject) => {
-  exec(`electrum --testnet broadcast ${transactionId}`, (err, data)=> {
-    if (err) {
-      reject(err);
-      }
-    
-      resolve(data);
-  });
-});
-
-
-broadcast('','').then(data => {
-  console.log(data);
-}).catch(err => console.error(err));
-
-
-getBalance().then(data => {
-  console.log(data);
-}).catch(err => console.error(err)); */
-
 
 const main = async () => {
   const walletData = await loadWallet();
+  const destinationAddress = 'tb1qtxrwmlx6whvhyzzjp7xrekdnytkj6qt2l4y8kx';
+  const amount = 0.0000001;
 
   if (walletData) {
     const newAddress = await createNewAddress();
@@ -129,7 +112,15 @@ const main = async () => {
   }
 
   const balance = await getBalance();
-  console.log(`Bakiyeniz: ${balance}`); 
+  console.log(`Bakiyeniz: ${balance}`);
+
+  const byteCode = await payto(destinationAddress, amount);
+  console.log(`Bytecode: ${byteCode}`);
+
+  if(byteCode){
+    const transactionId = await broadcast(byteCode);
+    console.log(`Transaction Id: ${transactionId}`);
+  }
 }
 
 main();
